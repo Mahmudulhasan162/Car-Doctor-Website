5328b5630be68a0afb4d4aa28fe9d0ec538b8d5f
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2024-01-07 19:01:01 +0600

    Footer Section initially done

---
+++ src/Components/Shared/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import logo from "../../../assets/icons/logo.svg"
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer pl-16 bg-[#151515] text-white h-[400px] items-center">
+  <aside>
+    <img className="text-white" src={logo} alt="" />
+    <p>Edwin Diaz is a software and web <br /> technologies engineer, a life coach <br />trainer who is also a serial .</p>
+  </aside> 
+  <nav>
+    <header className="footer-title">About</header> 
+    <a className="link link-hover">Home</a>
+    <a className="link link-hover">Services</a>
+    <a className="link link-hover">Contact</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Company</header> 
+    <a className="link link-hover">Why Car Doctor</a>
+    <a className="link link-hover">About</a>
+  </nav> 
+  <nav>
+    <header className="footer-title">Supports</header> 
+    <a className="link link-hover">Support Center</a>
+    <a className="link link-hover">Feedback</a>
+    <a className="link link-hover">Accessability</a>
+  </nav>
+</footer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Shared/Footer/Footer";
+import Navbar from "../Components/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
